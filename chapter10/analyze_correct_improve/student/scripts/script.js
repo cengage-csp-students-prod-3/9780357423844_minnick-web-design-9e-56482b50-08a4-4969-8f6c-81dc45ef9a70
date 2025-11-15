52f876e7b5063ad7a50dd41cc33dd2601ff26469
hannahwhisenant <hwhisenant.7664@wcstudents.wc.edu>
2025-11-15 03:56:25 +0000

    Testing Sat Nov 15 2025 03:56:25 GMT+0000 (Coordinated Universal Time)

--- chapter10/analyze_correct_improve/student/scripts/script.js
+++ chapter10/analyze_correct_improve/student/scripts/script.js
@@ -24,3 +24,11 @@ function pic2() {
     figElement.style.display = "block";
     figCap.textContent = "Coast of San Juan";
 }
+
+//Function to display the third picture
+function pic3() {
+    imgSource.src = "images/curacao.jpg";
+    imgSource.alt = "The blue waters of Curacao";
+    figElement.style.display = "block";
+    figCap.textContent = "Curacao";
+}

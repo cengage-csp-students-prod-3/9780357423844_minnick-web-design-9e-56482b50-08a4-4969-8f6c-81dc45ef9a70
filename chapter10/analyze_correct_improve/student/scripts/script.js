af55755b6f09d70f3daa11ccc51d3839a114864a
hannahwhisenant <hwhisenant.7664@wcstudents.wc.edu>
2025-11-15 03:33:46 +0000

    Testing Sat Nov 15 2025 03:33:46 GMT+0000 (Coordinated Universal Time)

--- chapter10/analyze_correct_improve/student/scripts/script.js
+++ chapter10/analyze_correct_improve/student/scripts/script.js
@@ -6,7 +6,8 @@
 
 //Function to display the first picture
 function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+    imgSource.src = "images/trunk-bay.jpg";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
+}

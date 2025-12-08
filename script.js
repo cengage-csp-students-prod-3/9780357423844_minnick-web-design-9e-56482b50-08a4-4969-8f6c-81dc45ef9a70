46b599da3d01370277a2af015aab668d44519eae
hannahwhisenant <hwhisenant.7664@wcstudents.wc.edu>
2025-12-07 20:02:29 -0600

    project

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 /*
-    Student Name: Hannah Whisenant
-    File Name: script.js
-    Date: December 6, 2025
+	Student Name: Hannah Whisenant
+	File Name: script.js
+	Date: 11/14/2025
 */
 
-//jQuery for hero image to consume the header window space
-$(document).ready(function(){
-	$('.hero').height($(window).height());
-});
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("example");
+var descriptionSource = document.getElementById("despsrc");
+
+//Hamburger menu function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("fcc-logo");
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display = "block";
+	} else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}

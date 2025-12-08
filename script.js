84e26edda7c84c7d04dd132622426aa368a7bab4
hannahwhisenant <hwhisenant.7664@wcstudents.wc.edu>
2025-12-07 18:21:59 -0600

    C12

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Hannah Whisenant
+    File Name: script.js
+    Date: December 6, 2025
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+	$('.hero').height($(window).height());
+});
